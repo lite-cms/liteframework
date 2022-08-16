bee5d834eba0f551f3967baa0f9ea39fbb02951d
ali <ali@persianicon.com>
2022-08-16 04:43:10 +0430

    added LiteFramework main php file

---
+++ liteframework/LiteFramework.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * LiteFramework
+ * Copyright (c) 2022 Ali Bakhtiar <https://persianicon.com/@ali>
+ * MIT License
+ * https://litecms.ir
+*/
+
+/**
+ * LiteFramework
+ *
+ * @modified : 16 Aug 2022
+ * @created  : 16 Aug 2022
+*/
+
+defined('LITEF_PATH') OR define('LITEF_PATH', rtrim(str_replace('\\', '/', __DIR__), '/'));
