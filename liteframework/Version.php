9c1fe89eca3bbab24aecc9fc1bb186fbc10cdaab
ali <ali@persianicon.com>
2022-08-16 04:48:28 +0430

    added version file

---
+++ liteframework/Version.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * LiteFramework
+ * Copyright (c) 2022 Ali Bakhtiar <https://persianicon.com/@ali>
+ * MIT License
+ * https://litecms.ir
+*/
+
+/**
+ * LiteFramework Version
+ *
+ * @modified : 16 Aug 2022
+ * @created  : 16 Aug 2022
+*/
+
+defined('LITEF_PATH') OR exit('Restricted access');
+
+define('LITEF_VERSION_MAJOR', 0);
+define('LITEF_VERSION_MINOR', 1);
+define('LITEF_VERSION_PATH' , 0);
+
+define('LITEF_RELEASE_DATE', '2022-08-16'); // Y-m-d
+
+define('LITEF_VERSION', LITEF_VERSION_MAJOR.'.'.
+	   LITEF_VERSION_MINOR.'.'.LITEF_VERSION_PATH);
