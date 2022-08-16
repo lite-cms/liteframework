8081c47b02d02d977263bb9d3c58c17d387bf02b
ali <ali@persianicon.com>
2022-08-16 05:53:19 +0430

    set internal character encoding to utf-8

--- liteframework/LiteFramework.php
+++ liteframework/LiteFramework.php
@@ -14,3 +14,14 @@
 */
 
 defined('LITEF_PATH') OR define('LITEF_PATH', rtrim(str_replace('\\', '/', __DIR__), '/'));
+
+/* Set internal character encoding to UTF-8 */
+if (function_exists('mb_internal_encoding') === false) {
+	http_response_code(500);
+	echo "'mbstring' extension is not loaded. This is required to run correctly.";
+	exit(1);
+}
+
+@ini_set('default_charset', 'UTF-8');
+mb_internal_encoding('UTF-8');
+mb_http_output('UTF-8');
